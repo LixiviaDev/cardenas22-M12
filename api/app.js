43938e29cff3cf5738f4b7023677cf3f46f4bf69
daw2-Cardenas22 <71781302+EricCardenasMartin@users.noreply.github.com>
2022-05-09 18:18:59 +0200

    :tada: Primera conexion de la api con sqlite :tada:

--- api/app.js
+++ api/app.js
@@ -1,14 +1,31 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+
 require('dotenv').config();
 const config = require('./config.js');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db.db');
 
-
-server.listen(config.PORT, () => {
+app.listen(config.PORT, () => {
   console.log(`Server running at http://${config.HOST}:${config.PORT}/`);
 });
+
+app.get('/', (req, res) => {
+    db.serialize(()=>{
+        db.all('SELECT * FROM prueba', (err,rows) => {     
+            if(err){
+                res.send("Error encountered while displaying");
+                return console.error(err.message);
+            }
+            let response = "";
+
+            for(let row of rows){
+                console.log(` ID: ${row.id},    Name: ${row.nombre}`);
+                response += ` ID: ${row.id},    Name: ${row.nombre}<br>`;
+            }
+
+            res.send(response);
+        });
+    });
+})
