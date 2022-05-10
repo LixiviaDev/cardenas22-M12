deb79d43a50fbac04322b751923fb1935584d1bf
daw2-Cardenas22 <71781302+EricCardenasMartin@users.noreply.github.com>
2022-05-10 18:01:16 +0200

    :construction: Adding bootstrap :construction:

---
+++ app/src/WebComponents/sharedInterface/SharedInterface.tsx
@@ -0,0 +1,107 @@
+import React from 'react'
+// import './SharedInterface.css'
+
+export default function SharedInterface(props: any) {
+    return(
+        <>
+        <header>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <a className="navbar-brand" href="#">Navbar</a>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a className="dropdown-item" href="#">Action</a></li>
+                        <li><a className="dropdown-item" href="#">Another action</a></li>
+                        <li><hr className="dropdown-divider"/></li>
+                        <li><a className="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link disabled" href="#" tabIndex={-1} aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                <form className="d-flex">
+                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button className="btn btn-outline-success" type="submit">Search</button>
+                </form>
+                </div>
+            </div>
+        </nav>
+        </header>
+
+        {props.children}
+
+        <footer className="py-5">
+            <div className="row">
+            <div className="col-2">
+                <h5>Section</h5>
+                <ul className="nav flex-column">
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+                </ul>
+            </div>
+
+            <div className="col-2">
+                <h5>Section</h5>
+                <ul className="nav flex-column">
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+                </ul>
+            </div>
+
+            <div className="col-2">
+                <h5>Section</h5>
+                <ul className="nav flex-column">
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+                </ul>
+            </div>
+
+            <div className="col-4 offset-1">
+                <form>
+                <h5>Subscribe to our newsletter</h5>
+                <p>Monthly digest of whats new and exciting from us.</p>
+                <div className="d-flex w-100 gap-2">
+                    <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
+                    <input id="newsletter1" type="text" className="form-control" placeholder="Email address"/>
+                    <button className="btn btn-primary" type="button">Subscribe</button>
+                </div>
+                </form>
+            </div>
+            </div>
+
+            <div className="d-flex justify-content-between py-4 my-4 border-top">
+            <p>© 2021 Company, Inc. All rights reserved.</p>
+            <ul className="list-unstyled d-flex">
+                <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"><use xlinkHref="#twitter"></use></svg></a></li>
+                <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"><use xlinkHref="#instagram"></use></svg></a></li>
+                <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"><use xlinkHref="#facebook"></use></svg></a></li>
+            </ul>
+            </div>
+        </footer>
+        </>
+    );
+}
