7fbe8990fc116dc67ffdafba36a423ac2027aa95
Lixívia <cuentadeveloper4@gmail.com>
2022-05-23 22:51:43 +0200

    :tada: El buscador ya busca (mal, pero busca) :tada:

--- app/src/Routes/Search/Search.tsx
+++ app/src/Routes/Search/Search.tsx
@@ -1,32 +1,78 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import { MangaBriefInfoData } from "../../TypeScript/Classes/Manga/MangaInfoData";
 import { BriefInfoMangaCard } from "../../WebComponents/Manga/MangaCard/MangaCard";
 import SharedInterface from "../../WebComponents/SharedInterface/SharedInterface";
+import configData from '../../config.json';
 
 export default function Search(props: any) {
     const [searchParams] = useSearchParams();
 
     const [search] = useState<string | null>(searchParams.get("search"));
 
+    const [searchResultsData, setSearchResultsData] = useState<MangaBriefInfoData[]>([]);
+
+    const [searchResults, setSearchResults] = useState<JSX.Element[]>([]);
+
+    useEffect(() => componentDidMount(), []);
+
+    useEffect(() => {
+                    if(searchResultsData.length > 0)
+                        generateSearchResults()
+                    }, [searchResultsData]);
+
+    function componentDidMount() {
+        if(search != null)
+            getChapterImagesData();
+    }
+
+    async function getChapterImagesData() {
+        setSearchResultsData(await fetchChapterImagesData());
+    }
+
+    async function fetchChapterImagesData(): Promise<MangaBriefInfoData[]>{
+        let bodyData = {
+                            token: localStorage.getItem("token"),
+                            search: search
+                        }
+
+        let body = {
+            method: 'POST',
+            mode: "cors" as RequestMode,
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(bodyData)
+        }
+
+        let res = await fetch(`${configData.API_URL}/${configData.ENDPOINTS.SEARCH_MANGA}`, body);
+        let data: MangaBriefInfoData[] = await res.json();
+
+        return data;
+    }
+
+    function generateSearchResults() {
+        let newSearchResults : JSX.Element[] = [];
+
+        for(let searchResultData of searchResultsData)
+            newSearchResults.push(<>
+                <div className="col-12 col-lg-6 p-2 d-flex" style={{height: "275px"}}>
+                    <BriefInfoMangaCard title={searchResultData.title}
+                                    img={searchResultData.image}
+                                    href={`/info/${searchResultData.mangaId}`}
+                                    sinopsis={searchResultData.sinopsis}
+                                    statusId={searchResultData.statusId}
+                                    lastChapter={searchResultData.lastChapter}
+                                    lastChapterDateAdded={searchResultData.lastChapterDateAdded} />
+                </div>
+            </>);
+        
+        setSearchResults(newSearchResults);
+    }
+
     return(
         <>
         <SharedInterface>
             <div className="row m-0 p-2">
-                <div className="col-12 col-lg-6 p-2 d-flex" style={{height: "275px"}}>
-                <BriefInfoMangaCard title="a"
-                                    img="a"
-                                    href="/a"/>
-                </div>
-                <div className="col-12 col-lg-6 p-2 d-flex" style={{height: "275px"}}>
-                <BriefInfoMangaCard title="a"
-                                    img="a"
-                                    href="/a"/>
-                </div>
-                <div className="col-12 col-lg-6 p-2 d-flex" style={{height: "275px"}}>
-                <BriefInfoMangaCard title="a"
-                                    img="a"
-                                    href="/a"/>
-                </div>
+                {searchResults}
             </div>
         </SharedInterface>
         </>
--- app/src/WebComponents/Manga/MangaCard/MangaCard.tsx
+++ app/src/WebComponents/Manga/MangaCard/MangaCard.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import './MangaCard.css';
+import { LanguageManager } from "../../../TypeScript/Managers/LanguageManager";
 
 export function HoverMangaCard(props: any) {
     const [title] = useState(props.title);
@@ -55,24 +56,28 @@ export function BriefInfoMangaCard(props: any){
     const [title] = useState(props.title);
     const [img] = useState(props.img);
     const [link] = useState(props.href);
+    const [sinopsis] = useState<string>(props.sinopsis);
+    const [statusId] = useState<string>(props.statusId);
+    const [lastChapter] = useState<string>(props.lastChapter);
+    const [lastChapterDateAdded] = useState<string>(props.lastChapterDateAdded);
 
-    const [description] = useState<string>(`The Jewel accidentally shatters into many bits which are dispersed across Japan when a beast from that age attempts to take the magical Shikon Jewel embodied in Kagome. and Kagome begin traveling to regain it before all the shards are found by the strong demon Naraku. Inuyasha and Kagome develop several allies throughout their journey, as Miroku, Shippo, Sango and Kirara. In sharp contrast to the usually comedic nature of much of the previous work of Takahashi, Inuyasha deals with subject matter that is darker, utilizing the setting of the Sengoku period to readily show the content that is violent.The narrative starts on the fifteenth birthday of Kagome Higurashi, a girl who lives with her mom, grandpa and little brother on the property of her family&apos;s hereditary Shinto shrine in Tokyo. When she goes to the good house to retrieve her cat, a centipede demon pulls the girl into it and bursts out of the enshrined Bone Eater&apos;s Well. The centipede demon is shown to have been after a magic jewel before being slain with a priestess named Kikyo, called the Shikon Jewel. Animated by the power in our time of the Shikon Jewel and mistaking her for Kikyo, the devil tries to kill the jewel to be gained by Kagome.The youth is shown by the nearby villagers to be , a half-dog demon who had been sealed by way of a perishing Kikyo fifty years back after being seemingly betrayed by her and trying to take the Shikon Jewel (which allows any wish the bearer want) to be able to be a full demon. After his dad&apos;s sword Tetsusaiga is gained by Inuyasha and is subdued by a charming necklace to keep him in line, he helps Kagome in coping with all the hazards they cause and gathering the shards.The two are joined in their own pursuit the somewhat revived Kikyo and from the young fox demon Shippo while coping with third parties groups like Inuyasha&apos;s older brother Sesshomaru, whose own version of what occurred years past brings the occasions into question. When joined a perverted monk whose bloodline is cursed, by Miroku, Kagome and Inuyasha learn the truth: the first disagreement between Kikyo and Inuyasha, disclosed to initially be fans, was due to a devious half-demon named Naraku. The developing Naraku is disclosed to have been born from your spirit of an evil guy named Onigumo inhabiting a body made within a pact by innumerable devils and who additionally put the hex on the family of Miroku. Sango, a devil slayer whose family was killed when her younger brother Kohaku fell under Naraku&apos;s control shortly joins after Inuyasha&apos;s group. Over time, Tetsusaiga abilities are enhanced by Inuyasha as he competes with Naraku&apos;s minion avatars like Kagura as well as the reanimated Band of Seven. The team of Inuyasha is broadly allied by Sesshomaru, Kikyo, as well as Koga who desires to avenge his comrades while flirting with Kagome was named by a wolf demon.While his heart is briefly removed by Naraku in the type of the Infant, who after tries to overthrow Naraku through his boat Moryomaru, Kohaku recovers his freewill and memories, as he tries to help out of remorse for indirectly killing his dad. With , matters settle throughout that time to empower his brother to its best capabilities to perfect Tetsusaiga. Eventually, Koga is made to stand on the sidelines, Kikyo posthumously uses the last of her capacity to give a second chance at life to Kohaku, and the Shikon Jewel is eventually reassembled by Naraku. The gem means to have so Naraku and she is going to be immobilized in battle for perpetuity, Kagome make a selfish wish. But for the Shikon Jewel, Kagome wishes with Inuyasha by her side to vanish. The actions, though, causes Kagome to go back together with the Well sealed to her time, inducing Inuyasha and her to lose contact for 3 years.Because point, the Sengoku period changes dramatically: Miroku and Sango have three kids together; Kohaku restarts his journey to be a devil slayer that is powerful as his company with Kirara; and the seventh position is attained by Shippo as a fox demon. Kagome returns to the Sengoku period where she becomes his wife and remains with .Other manga:`);
+    const [languageManager] = useState<LanguageManager>(LanguageManager.getInstance());
 
     return(<>
         <a href={link} className="w-100 h-100 bg-white border border-dark briefInfoMangaCard">
             <div className="bg-white h-100">
                 <div hidden>Click aqui para ver la informacion de {title}</div>
                 <div className="d-flex h-100">
-                    <img className="d-block d-sm-none m-auto ms-2 border border-dark" style={{width: "120px", height: "160px"}} src="https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/5930/9781593074845.jpg" alt="a" />
-                    <img className="d-none d-sm-block h-100" src="https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/5930/9781593074845.jpg" alt="a" />
+                    <img className="d-block d-sm-none m-auto ms-2 border border-dark" style={{width: "120px", height: "160px"}} src={img} alt="a" />
+                    <img className="d-none d-sm-block h-100" src={img} alt="a" style={{aspectRatio:"1/1.5"}}/>
                     <div className="w-100 h-100 p-2 d-flex flex-column text-black">
                         <div className="sectionTitle bg-black text-white p-2">
-                            <h2 className="m-0" style={{fontSize: "1.20rem"}}>SERIAL EXPERIMENTS LAIN: NIGHTMARE OF FABRICATION</h2>
+                            <h2 className="m-0" style={{fontSize: "1.20rem"}}>{title}</h2>
                         </div>
-                        <a className="w-100 py-2 ps-1 pe-2 text-dark d-flex justify-content-between grayBorderBottom" href="/a" style={{fontSize: "small"}}>
-                            <p className="m-0">Ch. 0</p>
-                            <p className="m-0">2022/5/15</p>
-                            <p className="m-0 d-none d-sm-block">Ongoing</p>
+                        <a className="w-100 py-2 ps-1 pe-2 text-dark d-flex justify-content-between grayBorderBottom" href={`${link}/a`} style={{fontSize: "small"}}>
+                            <p className="m-0">Ch. {lastChapter}</p>
+                            <p className="m-0">{languageManager.dateToShortLangString(lastChapterDateAdded)}</p>
+                            <p className="m-0 d-none d-sm-block">{statusId}</p>
                         </a>
                         <div className="h-100 m-0 p-2 ps-0 text-truncate text-wrap" style={{maxHeight: "9em"}}>
                             <p  className="fw-bold m-0 pb-1" 
@@ -80,7 +85,7 @@ export function BriefInfoMangaCard(props: any){
                                     Sinopsis:
                             </p>
                             <p  style={{fontSize: "14px", lineHeight: "normal"}}
-                                dangerouslySetInnerHTML={{__html: description}}></p>
+                                dangerouslySetInnerHTML={{__html: sinopsis}}></p>
                         </div>
                     </div>
                 </div>
